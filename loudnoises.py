12be247f3bfced1f083a3bba6a2656990eeeb99f
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-12 13:30:17 +0100

    Using old regex

--- loudnoises.py
+++ loudnoises.py
@@ -1,6 +1,6 @@
 from opsdroid.skills import match_regex
 import random
 
-@match_regex(r'^[A-Z\s]+$')
+@match_regex(r'(\b([A-Z]{2,}\s+)([A-Z]{2,})\b)|(\b[A-Z]{5,}\b)')
 def loudnoises(opsdroid, message):
     message.respond("http://i.imgur.com/auk7p8z.jpg")
