45ee04df3a3d652f675b1082c9576c2076f95d64
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-12 13:46:53 +0100

    Rename function

--- loudnoises.py
+++ loudnoises.py
@@ -2,5 +2,5 @@ from opsdroid.skills import match_regex
 import random
 
 @match_regex(r'/(\b([A-Z]{2,}\s+)([A-Z]{2,})\b)|(\b[A-Z]{5,}\b)/g')
-def loudnoises(opsdroid, message):
+def loud(opsdroid, message):
     message.respond("http://i.imgur.com/auk7p8z.jpg")
