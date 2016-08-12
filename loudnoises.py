16d7634b0844e2ba85ee4bc526c688e47d0934bd
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-12 13:25:35 +0100

    Trying different regex

--- loudnoises.py
+++ loudnoises.py
@@ -1,6 +1,6 @@
 from opsdroid.skills import match_regex
 import random
 
-@match_regex(r'^[^a-z]*$')
+@match_regex(r'^[A-Z\s]+$')
 def loudnoises(opsdroid, message):
     message.respond("http://i.imgur.com/auk7p8z.jpg")
