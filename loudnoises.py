71b96c74a6072906800058cc7338c00ea0248480
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-14 16:06:31 +0000

    Updates to be async

--- loudnoises.py
+++ loudnoises.py
@@ -2,5 +2,5 @@ from opsdroid.skills import match_regex
 import random
 
 @match_regex(r'(\b([A-Z]{2,}\s+)([A-Z]{2,})\b)|(\b[A-Z]{5,}\b)')
-def loud(opsdroid, message):
-    message.respond("http://i.imgur.com/auk7p8z.jpg")
+async def loud(opsdroid, message):
+    await message.respond("http://i.imgur.com/auk7p8z.jpg")
