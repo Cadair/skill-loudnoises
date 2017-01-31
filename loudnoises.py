5c99c7f0d541444dfb90afb0525b858ef31469b2
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2017-01-31 19:03:33 +0000

    Update for v0.6.0

--- loudnoises.py
+++ loudnoises.py
@@ -1,6 +1,6 @@
-from opsdroid.skills import match_regex
+from opsdroid.matchers import match_regex
 import random
 
 @match_regex(r'(\b([A-Z]{2,}\s+)([A-Z]{2,})\b)|(\b[A-Z]{5,}\b)')
-async def loud(opsdroid, message):
+async def loud(opsdroid, config, message):
     await message.respond("http://i.imgur.com/auk7p8z.jpg")
